08573227dfbbac47a3aff192ad5c28421e573c7d
wangluqiang <lq.wang@wisetimes.cn>
2025-02-05 17:41:14 +0800

    优化

--- DUIThink/DUIExport/DefineResource.h
+++ DUIThink/DUIExport/DefineResource.h
@@ -171,7 +171,7 @@ static std::pair<CMMString, CMMString> g_szPublicImage[]
 	{ Name_ImageClockSecond, _T("SkinDesign/ClockSecond.png") },
 	{ Name_ImageClockCenterDot, _T("SkinDesign/ClockCenterDot.png") },
 	{ Name_ImageGif, _T("SkinDesign/Face.gif") },
-	{ Name_ImageSequenceFrame, _T("SkinDesign/SequenceFrame.jpeg") },
+	{ Name_ImageSequenceFrame, _T("SkinDesign/SequenceFrame.png") },
 	{ Name_Imagebtn_normal, _T("SkinDesign/btn_normal.png") },
 	{ Name_Imagebtn_highlight, _T("SkinDesign/btn_highlight.png") },
 	{ Name_Imagebtn_down, _T("SkinDesign/btn_down.png") },
