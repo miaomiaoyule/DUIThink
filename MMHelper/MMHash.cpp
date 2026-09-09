14d58b07d45b39f406c9e923d2fcc1c6b50b82ef
wangluqiang <284168136@qq.com>
2026-09-09 15:18:36 +0800

    跨平台

--- MMHelper/MMHash.cpp
+++ MMHelper/MMHash.cpp
@@ -4,17 +4,21 @@
 //////////////////////////////////////////////////////////////////////////
 uint32_t CMMHash::GetHash(CMMString &str)
 {
-	return MM_Fnv1a_append_bytes(MM_FNV_offset_basis, (const unsigned char*)str.c_str(), sizeof(TCHAR) * str.length());
+	return MM_Fnv1a_append_lpctstr(MM_FNV_offset_basis, str.c_str(), (uint32_t)str.length());
 }
 
 uint32_t CMMHash::GetHash(LPCSTR lpszStr)
 {
-	return MM_Fnv1a_append_bytes(MM_FNV_offset_basis, (const unsigned char*)lpszStr, sizeof(CHAR) * strlen(lpszStr));
+	if (NULL == lpszStr) return MM_FNV_offset_basis;
+
+	return MM_Fnv1a_append_bytes(MM_FNV_offset_basis, (const unsigned char*)lpszStr, sizeof(CHAR) * (uint32_t)strlen(lpszStr));
 }
 
 uint32_t CMMHash::GetHash(LPCTSTR lpszStr)
 {
-	return MM_Fnv1a_append_bytes(MM_FNV_offset_basis, (const unsigned char*)lpszStr, sizeof(TCHAR) * lstrlen(lpszStr));
+	if (NULL == lpszStr) return MM_FNV_offset_basis;
+
+	return MM_Fnv1a_append_lpctstr(MM_FNV_offset_basis, lpszStr, (uint32_t)lstrlen(lpszStr));
 }
 
 uint32_t CMMHash::GetHash(const std::vector<CMMString> &vecStr)
--- MMHelper/MMHash.h
+++ MMHelper/MMHash.h
@@ -19,6 +19,51 @@ inline uint32_t MM_Fnv1a_append_bytes(uint32_t _Val,
 	return (_Val);
 }
 
+// Designer Attribute.xml ValueIDs are FNV-1a of UTF-16LE code units (Windows wchar_t).
+// Linux wchar_t is UTF-32; hashing native bytes would not match those IDs.
+inline uint32_t MM_Fnv1a_append_utf16le_units(uint32_t _Val, unsigned int cp)
+{
+	if (cp > 0xFFFF)
+	{
+		cp -= 0x10000;
+		const unsigned short high = (unsigned short)(0xD800 + (cp >> 10));
+		const unsigned short low = (unsigned short)(0xDC00 + (cp & 0x3FF));
+		const unsigned char buf[4] =
+		{
+			(unsigned char)(high & 0xFF), (unsigned char)(high >> 8),
+			(unsigned char)(low & 0xFF), (unsigned char)(low >> 8)
+		};
+	
+		return MM_Fnv1a_append_bytes(_Val, buf, 4);
+	}
+
+	const unsigned short cu = (unsigned short)cp;
+	const unsigned char buf[2] = { (unsigned char)(cu & 0xFF), (unsigned char)(cu >> 8) };
+	return MM_Fnv1a_append_bytes(_Val, buf, 2);
+}
+
+inline uint32_t MM_Fnv1a_append_lpctstr(uint32_t _Val, LPCTSTR psz, uint32_t nChars)
+{
+	if (NULL == psz || 0 == nChars) return _Val;
+
+#ifdef UNICODE
+	for (uint32_t i = 0; i < nChars; ++i)
+	{
+		unsigned int cp = (unsigned int)psz[i];
+		if (sizeof(TCHAR) == 2)
+		{
+			cp &= 0xFFFFu;
+		}
+	
+		_Val = MM_Fnv1a_append_utf16le_units(_Val, cp);
+	}
+	
+	return _Val;
+#else
+	return MM_Fnv1a_append_bytes(_Val, (const unsigned char *)psz, nChars);
+#endif
+}
+
 //////////////////////////////////////////////////////////////////////////
 class MMHELPER_API CMMHash
 {
@@ -40,7 +85,7 @@ namespace std
 		{
 			//forbid CMMHash::GetHash(str) to avoid dependency, and use FNV-1a algorithm directly here
 			//if CMMHash::GetHash(str) connot delay load MMHelper dll
-			return MM_Fnv1a_append_bytes(MM_FNV_offset_basis, (const unsigned char*)str.c_str(), sizeof(TCHAR) * str.length());
+			return MM_Fnv1a_append_lpctstr(MM_FNV_offset_basis, str.c_str(), (uint32_t)str.length());
 		}
 	};
 }
